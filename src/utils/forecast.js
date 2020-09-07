34291fff238288a455796a3d7642ca0a08c63c0b
hem joshi <hemjoshi8055@gmail.com>
2020-09-07 14:34:12 +0530

    Forecast change

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longtitude, callback) => {
             callback('Unable to find location')
         }
         else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + '. Feels like ' + body.current.feelslike + ' degree')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '%.')
         }
     })
 }
